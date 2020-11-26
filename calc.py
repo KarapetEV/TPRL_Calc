cf805f7215c1298e2b8d331a4ae741cafac57d38
Evgeniy Karapyshev <karapet2011@gmail.com>
2020-11-27 02:39:54 +0300

    переделан GUI добавлен метод, выделяющий уровень слайдером

--- calc.py
+++ calc.py
@@ -1,13 +1,42 @@
-# Тут будет крутой калькулятор
+import sys
+from calc_window import *
 from PyQt5 import QtCore, QtWidgets, uic, QtGui
+
 
 class Window(QtWidgets.QWidget):
     def __init__(self, parent=None):
         QtWidgets.QWidget.__init__(self, parent)
-        uic.loadUi("firstForm.ui", self)
+        self.ui = Ui_AppWindow()
+        self.ui.setupUi(self)
+        self.ui.ugtSlider.valueChanged.connect(self.change_ugt_level)
+
+    def change_ugt_level(self):
+        labels_ugt = {
+            self.ui.label_ugt0: 0,
+            self.ui.label_ugt1: 1,
+            self.ui.label_ugt2: 2,
+            self.ui.label_ugt3: 3,
+            self.ui.label_ugt4: 4,
+            self.ui.label_ugt5: 5,
+            self.ui.label_ugt6: 6,
+            self.ui.label_ugt7: 7,
+            self.ui.label_ugt8: 8,
+            self.ui.label_ugt9: 9,
+        }
+        size = self.ui.ugtSlider.value()
+        for k, v in labels_ugt.items():
+            font = QtGui.QFont()
+            if v == size:
+                font.setPointSize(16)
+                k.setFont(font)
+                k.setEnabled(True)
+            else:
+                font.setPointSize(12)
+                k.setFont(font)
+                k.setEnabled(False)
+
 
 if __name__ == '__main__':
-    import sys
     app = QtWidgets.QApplication(sys.argv)
     window = Window()                   # Создаем экземпляр класса
     window.setWindowTitle('TRL Calculator')
