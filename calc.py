51e0a6690eaf226887803f4dd7f059837965efeb
Sadje <karapyshev@gmail.com>
2020-11-27 00:03:14 +0300

    similar command on pushButton installed

--- calc.py
+++ calc.py
@@ -6,6 +6,15 @@ class Window(QtWidgets.QWidget, ui_firstForm.Ui_FormParams):
     def __init__(self, parent=None):
         QtWidgets.QWidget.__init__(self, parent)
         self.setupUi(self)
+        self.set_params.clicked.connect(self.on_clicked)
+
+    def on_clicked(self):
+        text = self.set_params.text()
+        if text == "Установить параметры":
+            self.set_params.setText("Параметры установлены")
+        else:
+            self.set_params.setText("Установить параметры")
+
 
 if __name__ == '__main__':
     import sys
