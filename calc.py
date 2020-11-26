37b5db8266b3abf0b776a5c80f0dfc9b5308f0b2
Sadje <karapyshev@gmail.com>
2020-11-26 21:31:00 +0300

    PyQt Form 1 designed

--- calc.py
+++ calc.py
@@ -1,9 +1,17 @@
 # Тут будет крутой калькулятор
+from PyQt5 import QtCore, QtWidgets, uic, QtGui
 
+class Window(QtWidgets.QWidget):
+    def __init__(self, parent=None):
+        QtWidgets.QWidget.__init__(self, parent)
+        uic.loadUi("firstForm.ui", self)
 
-def func(x, y):
-    print(x + y)
-
-
-if __name__ == "__main__":
-    func(2, 3)
+if __name__ == '__main__':
+    import sys
+    app = QtWidgets.QApplication(sys.argv)
+    window = Window()                   # Создаем экземпляр класса
+    window.setWindowTitle('TRL Calculator')
+    window.setWindowIcon(QtGui.QIcon('rjd.png'))
+    # window.resize(300,70)
+    window.show()
+    sys.exit(app.exec_())
