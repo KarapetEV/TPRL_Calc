64cc0a60d53a0d41b04bf7f99f71e292b75cc7cc
KarapetEV <karapet2011@gmail.com>
2021-01-13 12:55:24 +0300

    добавлено окно заставки

--- splash.py
+++ splash.py
@@ -7,18 +7,18 @@ class Splash(QSplashScreen):
 
     def __init__(self):
         super(Splash, self).__init__()
+        self.setFixedSize(500, 400)
         self.setPixmap(QPixmap("img/splash_img.jpg"))
 
         self.label_title = QLabel(self)
-        self.label_title.setGeometry(0, 20, 500, 100)
+        self.label_title.setGeometry(0, 30, 500, 100)
         self.label_title.setPixmap(QPixmap("img/splash_appname.png"))
-
+        self.label_msg = QLabel(self)
+        self.label_msg.setGeometry(200, 150, 100, 100)
+        self.label_msg.setText("Скоро поедем...")
         self.label_animation = QLabel(self)
+        self.label_animation.setGeometry(200, 250, 100, 100)
         self.movie = QMovie("img/load1.gif")
+        self.label_animation.setScaledContents(True)
         self.label_animation.setMovie(self.movie)
         self.movie.start()
-
-app = QApplication(sys.argv)
-splash = Splash()
-splash.show()
-sys.exit(app.exec_())
